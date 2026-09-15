3049c2433e3a4de08334c08c4e148bbca5dfe0d6
twofaycee <twofaycee@gmail.com>
2026-09-15 12:28:29 -0700

    Finalize BREAKTHROUGH branded auth release

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,12 @@ export const metadata = {
   icons: { icon: '/brand/breakthrough-mark.svg' },
 }
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: '#050505',
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return <html lang="en"><body>{children}</body></html>
 }
